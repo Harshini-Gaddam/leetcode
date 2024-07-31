c18e72fe5e821d81af5ca84103b2916fad287874
Harshini-Gaddam <123286604+Harshini-Gaddam@users.noreply.github.com>
2024-07-31 17:11:29 -0400

    Time: 335 ms (62.99%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,4 +1,4 @@
 -- Write your PostgreSQL query statement below
-select name, bonus
+select e.name, b.bonus
 from employee as e left join bonus as b on e.empId = b.empId
 where bonus<1000 or bonus is null
